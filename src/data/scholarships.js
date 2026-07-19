1e75db457fe699f5d71583574728c4dcf65f2484
xuhaahmed <zuha5203@gmail.com>
2026-07-20 00:53:26 +0500

    Restored scholarship database

---
+++ src/data/scholarships.js
@@ -0,0 +1,61 @@
+const scholarships = [
+
+  {
+    id: 1,
+    name: "Global Korea Scholarship (GKS)",
+    country: "South Korea",
+    university: "Various Korean Universities",
+    funding: "Fully Funded",
+    deadline: "September",
+    field: "Computer Science",
+    minCGPA: 3.0,
+    description:
+      "A fully funded scholarship by the Korean government for international students pursuing undergraduate and graduate studies."
+  },
+
+
+  {
+    id: 2,
+    name: "MEXT Scholarship",
+    country: "Japan",
+    university: "Japanese Universities",
+    funding: "Fully Funded",
+    deadline: "April",
+    field: "Computer Science",
+    minCGPA: 3.2,
+    description:
+      "Japanese government scholarship providing financial support for international students."
+  },
+
+
+  {
+    id: 3,
+    name: "Chinese Government Scholarship (CSC)",
+    country: "China",
+    university: "Chinese Universities",
+    funding: "Fully Funded",
+    deadline: "March",
+    field: "Computer Science",
+    minCGPA: 3.0,
+    description:
+      "Scholarship program offered by the Chinese government for international students."
+  },
+
+
+  {
+    id: 4,
+    name: "DAAD Scholarship",
+    country: "Germany",
+    university: "German Universities",
+    funding: "Fully Funded",
+    deadline: "October",
+    field: "Computer Science",
+    minCGPA: 3.3,
+    description:
+      "A prestigious scholarship program for international students studying in Germany."
+  }
+
+];
+
+
+export default scholarships;
